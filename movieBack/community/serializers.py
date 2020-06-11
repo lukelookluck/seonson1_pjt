125335fe5f855622fe2b15142976c0c48434d20a
gunhee9177 <gunhee9177@naver.com>
2020-06-11 17:41:00 +0900

    hahaha

--- movieBack/community/serializers.py
+++ movieBack/community/serializers.py
@@ -17,4 +17,4 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('id', 'user', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'user', 'created_at', 'updated_at', 'DISLIKE', 'LIKE')
